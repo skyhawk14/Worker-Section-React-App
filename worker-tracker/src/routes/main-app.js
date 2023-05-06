d9a5a6a289412c90b492b925cfa867e712946518
Mani <bhushan.mani72@gmail.com>
2023-05-06 13:06:56 +0000

    modify routing for handling home route

--- worker-tracker/src/routes/main-app.js
+++ worker-tracker/src/routes/main-app.js
@@ -1,29 +1,24 @@
-import { Outlet, useLoaderData } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import WebsiteAppBar from "../AppBar";
-import { forwardRef, useState } from "react";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase/setup";
+import ErrorPage from "./error-page";
 
-const Alert = forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
 export default function MainApp() {
-  
-  // ToDo: set global error and success using global state
-  const [open, setOpen] = useState(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
-  };
-
+  const [isLogin, setIsLogin] = useState(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLogin(true);
+      } else {
+        return navigate("/login");
+      }
+    });
+  }, [isLogin]);
+  if (!isLogin) {
+    return null;
+  }
   return (
     <>
       <WebsiteAppBar />
